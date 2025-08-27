23ded79e229617ee55e02b8d5c6c2f01707ae919
Egle Vondrak <egle.vondrak@gmail.com>
2025-08-27 08:43:36 +0200

    Fix Homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,14 @@
+import { Navigate } from 'react-router-dom';
+import { useAuthStore } from '../store/useAuthStore';
 import AllPostsPage from './AllPostsPage';
 
 function Homepage() {
+    const user = useAuthStore((state) => state.user);
+
+    if (user) {
+        return <Navigate to='/app' replace />;
+    }
+
     return (
         <main className='w-full bg-[rgb(var(--color-bg))] text-[rgb(var(--color-text))] pt-3 mb-4 px-2'>
             <AllPostsPage />
